ed6bc44f09050d777ffd4452acde5fe716ce6032
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:03:16 +0300

    [s/03_biblio] Init

---
+++ server/app/03_biblio/content.php
@@ -0,0 +1,29 @@
+<?php
+require("db_connect.php");
+?>
+
+<div class="content">
+<table border=1>
+<th>id</th>
+<th>title</th>
+<th>author</th>
+<th>actions</th>
+<?php
+$s = "select * from book";
+$res = mysqli_query($con, $s);
+
+while($row = mysqli_fetch_array($res)) {
+    print("<tr>");
+    printf("<td>%s</td>", $row[0]);
+    printf("<td>%s</td>", $row["title"]);
+    printf("<td>%s</td>", $row[2]);
+
+    print("<td>");
+    printf('<a href="delete.php?id=%s">delete</a>', $row[0]);
+    print("</td>");
+
+    print("</tr>");
+}
+?>
+</table>
+</div>
---
+++ server/app/03_biblio/index.php
@@ -0,0 +1,6 @@
+<?php
+require("header.html");
+require("menu.html");
+require("content.php");
+require("footer.html");
+?>
