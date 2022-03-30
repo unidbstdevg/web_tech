8fcf5de1250a3cefd71639d92faa8e41ac09d44b
Evgeny <sev.20@uni-dubna.ru>
2022-03-30 22:03:16 +0300

    [01_credit] Add design

--- server/app/01_credit/index.php
+++ server/app/01_credit/index.php
@@ -1,3 +1,11 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="styles.css" rel="stylesheet">
+        <title>Credit</title>
+    </head>
+    <body>
 <?php
 if (isset($_GET["S"]) && isset($_GET["P"]) && isset($_GET["N"])){
     $S = (float)$_GET["S"];
@@ -7,8 +15,10 @@ if (isset($_GET["S"]) && isset($_GET["P"]) && isset($_GET["N"])){
     printf("Для суммы %s со ставкой %s<br>", $S, $P);
 
     print("<table>");
+    print("<tr><th>Месяц</th><th>Платёж</th></tr>");
     for($i = 1; $i <= $N; $i++) {
         $x = $S * ($P + ($P / (((1 + $P) ** $i) - 1)));
+        $x = round($x, 2);
 
         print("<tr>");
         printf("<td>%s</td><td>%s</td>", $i, $x);
@@ -19,19 +29,20 @@ if (isset($_GET["S"]) && isset($_GET["P"]) && isset($_GET["N"])){
     printf("<br>");
 }
 ?>
-<html>
-    <body>
-        <form method="get" action="./">
-            <span>Первоначальная сумма кредита</div>
-            <input type="text" name="S">
-            <br>
-            <span>Ежемесячная ставка по процентам</div>
-            <input type="text" name="P">
-            <br>
-            <span>На сколько месяцев</div>
-            <input type="text" name="N">
-            <br>
-            <input type="submit" value="Тыкни меня">
-        </form>
+    <form method="get" action="./">
+        <span>Первоначальная сумма кредита</div>
+        <input type="text" name="S">
+        <br>
+        <span>Ежемесячная ставка по процентам</div>
+        <input type="text" name="P">
+        <br>
+        <span>На сколько месяцев</div>
+        <input type="text" name="N">
+        <br>
+        <input type="submit" value="Тыкни меня">
+    </form>
+    <br>
+    <audio src="sound.mp3" controls></audio>
+
     </body>
 </html>
